174faec86acfdc135bc3434daf3c7f3cc82e9793
태연 <s2401@e-mirim.hs.kr>
2026-03-20 14:42:39 +0900

    JS

--- JS/0320.js
+++ JS/0320.js
@@ -2,3 +2,12 @@
 // let [a, b, c] = colors;
 // console.log(a, b, c)
 
+// const words = ["apple", "kiwi", "banana", "cat"]
+// const res = words.filter(word => word.length >= 5)
+// console.log(res)
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const sum = numbers
+  .filter(num => num % 2 === 0)
+  .reduce((acc, num) => acc + num, 0);
+console.log(sum);
